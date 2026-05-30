fa9ed3a56bb72d6edc6d310ccd47c2b981d2279e
AlfaSamca <79315532+AlfaSamca@users.noreply.github.com>
2026-05-30 15:11:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -462,7 +462,8 @@ async def set_time(callback: CallbackQuery, state: FSMContext):
 
     await callback.message.edit_text("В каком зале вы бы хотели отдохнуть?", reply_markup=kb.as_markup())
     await state.set_state(Booking.floor)
-    
+
+
 # ШАГ 2: Отправка фото-схемы выбранного этажа и интерактивной клавиатуры столов
 # ШАГ 2: Отправка фото-схемы выбранного этажа и интерактивной клавиатуры столов
 @dp.callback_query(F.data.startswith("floor:"))
